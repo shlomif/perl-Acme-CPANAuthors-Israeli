e25efabc7372b2419cdcd78d833095a608b241dc
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-09-14 12:49:22 +0000

    Added the skeleton as generated by Module-Starter. r3200!.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/berlios/TO_DEL__UNPACK/web-cpan/Acme-CPANAuthors-Israeli/trunk@3200 52c325ad-5fd0-0310-8a0f-c43feede02cc

---
+++ Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
@@ -0,0 +1,106 @@
+package Acme::CPANAuthors::Israeli;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Acme::CPANAuthors::Israeli - The great new Acme::CPANAuthors::Israeli!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Acme::CPANAuthors::Israeli;
+
+    my $foo = Acme::CPANAuthors::Israeli->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-acme-cpanauthors-israeli at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-CPANAuthors-Israeli>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Acme::CPANAuthors::Israeli
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-CPANAuthors-Israeli>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Acme-CPANAuthors-Israeli>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Acme-CPANAuthors-Israeli>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Acme-CPANAuthors-Israeli>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: mit
+
+
+=cut
+
+1; # End of Acme::CPANAuthors::Israeli
