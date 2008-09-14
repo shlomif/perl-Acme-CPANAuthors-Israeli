e68fac8b92c86319be45aab63186cfe5c6af7853
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2008-09-14 14:07:34 +0000

    Bumped the version number.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/berlios/TO_DEL__UNPACK/web-cpan/Acme-CPANAuthors-Israeli/trunk@3209 52c325ad-5fd0-0310-8a0f-c43feede02cc

--- Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
+++ Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
@@ -9,7 +9,7 @@ Acme::CPANAuthors::Israeli - We are Israeli CPAN Authors
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.0101';
 
 use Acme::CPANAuthors::Register (
     AMOSS => 'Amos Shapira',
