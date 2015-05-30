65c3854631936f7fe0ce141710b5a676816e200f
Shlomi Fish <shlomif@shlomifish.org>
2015-05-30 11:13:41 +0300

    Bump version

--- Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
+++ Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
@@ -9,7 +9,7 @@ Acme::CPANAuthors::Israeli - We are Israeli CPAN Authors
 
 =cut
 
-our $VERSION = '0.0200';
+our $VERSION = '0.0201';
 
 use Acme::CPANAuthors::Register (
     AMOSS => 'Amos Shapira',
