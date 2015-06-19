c92eb25f9a9bb815568ae81e59edaf78ee1cc732
Shlomi Fish <shlomif@shlomifish.org>
2015-06-19 14:31:28 +0300

    Convert to dzil

--- Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
+++ Acme-CPANAuthors-Israeli/lib/Acme/CPANAuthors/Israeli.pm
@@ -57,60 +57,9 @@ This class provides a hash of Pause IDs/names of Israeli CPAN authors.
 If you are an Israeli CPAN author and are not listed here, please contact
 me.
 
-=head1 AUTHOR
-
-Shlomi Fish, C<< <shlomif at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-cpanauthors-israeli at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-CPANAuthors-Israeli>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::CPANAuthors::Israeli
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-CPANAuthors-Israeli>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-CPANAuthors-Israeli>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-CPANAuthors-Israeli>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-CPANAuthors-Israeli>
-
-=item * Version Control Repository:
-
-L<https://svn.berlios.de/svnroot/repos/web-cpan/Acme-CPANAuthors-Israeli/>
-
-=back
-
 =head1 SEE ALSO
 
 L<Acme::CPANAuthors> - the driver for this class.
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Shlomi Fish, all rights reserved.
-
-This program is released under the following license: MIT/X11 License.
 
 =cut
 
